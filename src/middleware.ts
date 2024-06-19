9d50710df6624ddfb57fd0466d0b2e7dc685e747
Suranjit mondal <suranjitm07@gmail.com>
2024-06-20 05:47:04 +0600

    MeduilWare & sign-in set-up

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,25 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
- 
+export { default } from "next-auth/middleware"
+import { getToken } from "next-auth/jwt"
 // This function can be marked `async` if using `await` inside
-export function middleware(request: NextRequest) {
-  return NextResponse.redirect(new URL('/home', request.url))
+export async function middleware(request: NextRequest) {
+    const token = await getToken({req:request})
+    const url = request.nextUrl
+    if(token && (
+        url.pathname.startsWith("/sign-in")||
+        url.pathname.startsWith("/sign-up")||
+        url.pathname.startsWith("/verify")||
+        url.pathname.startsWith("/")
+    ))
+return NextResponse.redirect(new URL('/home', request.url))
 }
- 
+
 // See "Matching Paths" below to learn more
 export const config = {
-  matcher: '/about/:path*',
+matcher: [
+    '/sign-in',
+    '/sign-up',
+
+]
 }
